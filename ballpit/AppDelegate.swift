7c599ebc45c20ffcc53052bd18dfd8edd7b2dd99
mortenjust <morten.just@genstart.dk>
2016-02-05 15:54:09 -0800

    Initial Commit

---
+++ ballpit/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ballpit
+//
+//  Created by Morten Just Petersen on 2/5/16.
+//  Copyright © 2016 Morten Just Petersen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
